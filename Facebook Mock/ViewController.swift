83f3b35acb2658507002cd2beb689e6b6f51fcbe
Venkatram G V <venkat.gv@zohocorp.com>
2022-12-07 12:22:19 +0530

    Initial Commit

---
+++ Facebook Mock/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Facebook Mock
+//
+//  Created by Venkatram G V on 07/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
